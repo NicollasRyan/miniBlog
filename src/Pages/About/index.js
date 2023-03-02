d78e660e6764cab5490e32a73ada19f440dc6492
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-03-02 13:40:05 -0300

    Bloqueando páginas que precisam de autenticação

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom";
+
 import styles from "./styles.module.css";
 
 export function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h1>
+        Sobre o Mini <span>Blog</span>
+      </h1>
+      <p>
+        Este projeto consiste em um blog feito com react no front-end e firebase
+        no back-end
+      </p>
+      <Link to="/posts/create" className="btn">
+        Criar post
+      </Link>
     </div>
   );
 }
