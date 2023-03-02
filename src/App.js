71fa2efd5dc68e1de04c6904366cc868709eb723
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-03-02 05:32:09 -0300

    Links para usuário autenticado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 
 import { AuthProvider } from "./context/AuthContext";
 
@@ -14,11 +18,28 @@ import { About } from "./Pages/About";
 import { Home } from "./Pages/Home";
 import { Login } from "./Pages/Login";
 import { Register } from "./Pages/Register";
+import { CreatePost } from "./Pages/CreatePost";
+import { Dashboard } from "./Pages/Dashbord";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={{ user }}>
         <Router>
           <Navbar />
           <div className="container">
@@ -27,6 +48,8 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/posts/create" element={<CreatePost />} />
+              <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
           </div>
           <Footer />
