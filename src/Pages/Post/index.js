a17ab62158538c45738545eb9e2109efa863c668
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-03-06 04:55:03 -0300

    Exibindo dado de post individual

--- src/Pages/Post/index.js
+++ src/Pages/Post/index.js
@@ -1,12 +1,20 @@
 import styles from "./styles.module.css";
 
 import { useParams } from "react-router-dom";
+import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 export function Post() {
   const { id } = useParams();
+  const { document: post, loading } = useFetchDocument("posts", id);
+
   return (
     <div>
-      <h1>Post: {id}</h1>
+      {loading && <p>Carregando...</p>}
+      {post && (
+        <>
+          <h1>{post.title}</h1>
+        </>
+      )}
     </div>
   );
 }
